cb159566c02dafe8df13cac2b4afbcb220f49a07
Austin Hallock <austinhallock@gmail.com>
2022-12-16 19:42:14 -0800

    disable react target pin for now

--- server/query.go
+++ server/query.go
@@ -430,14 +430,14 @@ func query(devMode bool) rex.Handle {
 			// FIXME: react is pinned to es2022 until oscar supports separate bundles for different es
 			// otherwise bundled vs non-bundled code potentially pull in different react versions.
 			// eg bundled always uses es2022, but opera/edge use es2021
-			if reqPkg.Name == "react" || reqPkg.Name == "react-dom" {
-				// es2022 instead of es2015 bc we have a bunch of old bundles that have es2022 in them.
-				// i don't *think* anything will break in older browsers, since react code prob doesn't
-				// distribute new es features anyways
-				target = "es2022"
-			} else {
-				target = getTargetByUA(ctx.R.UserAgent())
-			}
+			// if reqPkg.Name == "react" || reqPkg.Name == "react-dom" {
+			// 	// es2022 instead of es2015 bc we have a bunch of old bundles that have es2022 in them.
+			// 	// i don't *think* anything will break in older browsers, since react code prob doesn't
+			// 	// distribute new es features anyways
+			// 	target = "es2022"
+			// } else {
+			target = getTargetByUA(ctx.R.UserAgent())
+			// }
 		}
 
 		buildVersion := VERSION
